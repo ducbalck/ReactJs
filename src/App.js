1e8a6cac217c9a5690d95013496037bf75d285ac
ducblack <tongminhduc237@gmail.com>
2021-10-16 21:59:19 +0700

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
 import Menu from "./components/MenuComponent";
-
+import { DISHES } from './shared/dishes';
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
-    return <Menu />;
+    return <Menu dishes={this.state.dishes} />
+    ;
   }
 }
 
