a715619ec0ef5ce679e1fca0fdedad1f3c251c48
ducblack <tongminhduc237@gmail.com>
2021-10-20 10:17:26 +0700

    ex2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -7,6 +7,8 @@ import {
   CardBody,
   CardTitle,
 } from "reactstrap";
+import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const  DishDetail = (props) => {
   
@@ -14,8 +16,23 @@ const  DishDetail = (props) => {
       return (
         <div className="container">
           <div className="row">
-            <RenderDish dish={props.dish} />
-            <RenderComments dish={props.dish} />
+            <Breadcrumb>
+
+                <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+                <h3>{props.dish.name}</h3>
+                <hr />
+            </div>                
+          </div>
+          <div className="row">
+            <div className="col-12 col-md-5 m-1">
+                <RenderDish dish={props.dish} />
+            </div>
+            <div className="col-12 col-md-5 m-1">
+                <RenderComments comments={props.comments} />
+            </div>
           </div>
         </div>
       );
@@ -26,7 +43,7 @@ const  DishDetail = (props) => {
 function RenderDish({ dish }) {
   if (dish != null) {
     return (
-      <div className="col-12 col-md-5 m-1">
+      <div className="col-12  m-1">
           
         <Card> 
           <CardImg top src={dish.image} alt={dish.name} />
@@ -40,20 +57,22 @@ function RenderDish({ dish }) {
   } else return null;
 }
 
-function RenderComments({ dish }) {
-    if (dish != null) {
+function RenderComments({ comments }) {
+    if (comments != null) {
       return (
-        <div className="col-12 col-md-5 m-1">
-            <h4>Comments</h4>
-          {dish.comments.map((comment) => {
+        <div className="col-12  m-1">
+          <h4>Comments</h4>
+          <ul className="list-unstyled">
+          {comments.map((comment) => {
             return (
                 
-              <div>
+              <li key={comment.id}>
                 <p>{comment.comment}</p>
-                <p>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-              </div>
+                <p>--{comment.author},{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+              </li>
             );
           })}
+          </ul>
         </div>
       );
     } else return null;
