61eda23822a30b32591222f06c97f17d636df041
ducblack <tongminhduc237@gmail.com>
2021-10-21 20:50:28 +0700

    Controlled Forms

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'font-awesome/css/font-awesome.css';
-import 'bootstrap-social/bootstrap-social.css';
+// import 'bootstrap-social/bootstrap-social.css';
 
 ReactDOM.render(
   <React.StrictMode>
