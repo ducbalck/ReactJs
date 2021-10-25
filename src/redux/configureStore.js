e11a3c5ae62a253f98253781f75f0e734bfd0238
ducblack <tongminhduc237@gmail.com>
2021-10-25 22:34:29 +0700

    Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,7 @@
 import {createStore, combineReducers} from 'redux';
 import { Dishes } from './dishes';
 import { Comments } from './comments';
-import { Promotions } from './Promotions';
+import { Promotions } from './promotions';
 import { Leaders } from './leaders';
 
 export const ConfigureStore = () => {
